c45184e025544b78bba237faa5d3476940029742
b_jeptum <brendajeptum@gmail.com>
2024-11-06 00:51:50 +0300

    Basic Flask app updated

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -9,9 +9,9 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def home():
+def home() -> str:
     """Home Page"""
-    return render_template('index.html')
+    return render_template('0-index.html')
 
     if __name__ == '__main__':
         app.run(debug=True)
