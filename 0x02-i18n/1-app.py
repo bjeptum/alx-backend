6f6c945aa12f87cccbf9097aaf644b46f2525577
b_jeptum <brendajeptum@gmail.com>
2024-11-08 23:08:58 +0300

    Updated module documentation#2

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,21 +1,25 @@
 #!/usr/bin/env python3
 """
 Simple flask app
+Supports two languages: English and French
+Uses Flask-Babel to manage localization
 """
 from flask import Flask, render_template
 from flask_babel import Babel, _
 
 
+# Flask application setup
 app = Flask(__name__)
 
 
 class Config:
     """Config class to store Babel configuration"""
-    LANGUAGE = ['en', 'fr']
+    LANGUAGES= ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
+# Configure the Flask app with defined settings
 app.config.from_object(Config)
 babel = Babel(app)
 
