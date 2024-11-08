4bd0104a49e9c153dbae15f54fec5c2a9c59de6f
b_jeptum <brendajeptum@gmail.com>
2024-11-09 00:33:18 +0300

    Locale decorator updated#3

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -5,7 +5,7 @@ translations
 Uses Accept-Language header to infer user's
 language preference
 """
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_babel import Babel, _
 
 
@@ -19,19 +19,19 @@ class Config:
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
-    # Configure the Flask app with defined settings
-    app.config.from_objects(Config)
-    babel = Babel(app)
+# Configure the Flask app with defined settings
+app.config.from_objects(Config)
+babel = Babel(app)
 
-    @babel.localeselector
-    def get_locale() -> str:
-        """Select locale based on best match"""
-        return request.accept_languages.best_match(app.config['LANGUAGES'])
+@babel.localeselector
+def get_locale() -> str:
+    """Select locale based on best match"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
-    @app.route('/')
-    def home() -> str:
-        """Render Home Page"""
-        return render_template('2-index.html')
+@app.route('/')
+def home() -> str:
+    """Render Home Page"""
+    return render_template('2-index.html')
 
-    if__name__ == "__main__":
-        app.runt(debug=True)
+if __name__ == "__main__":
+    app.runt(debug=True)
