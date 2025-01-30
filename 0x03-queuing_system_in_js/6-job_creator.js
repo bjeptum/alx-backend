af291b7a3be7f09033e341560b92377b19aab230
b_jeptum <brendajeptum@gmail.com>
2025-01-30 21:50:43 +0300

    Create the Job creator

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,19 @@
+const kue = require('kue');
+const queue = kue.createQueue();
+
+const job = queue.create('push_notification_code', {
+    phoneNumber: '254-703-45678',
+    message: 'Hello'
+}).save((err) => {
+    if (!err) {
+        console.log(`Notification job created: ${job.id}`);
+    }
+});
+
+job.on('completed', () => {
+    console.log('Notification job completed');
+});
+
+job.on('failed', (err) => {
+    console.log('Notification job failed', err);
+});
